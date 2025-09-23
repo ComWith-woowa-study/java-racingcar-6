1743f33921dc85c4a79d7ea5957bc6a5e500eb4d
smini <sm020320@naver.com>
2025-09-23 19:59:07 +0900

    feat: Race 구현

---
+++ src/main/java/racingcar/model/Race.java
@@ -0,0 +1,42 @@
+package racingcar.model;
+
+import racingcar.model.car.RacingCar;
+import racingcar.model.io.InputView;
+import racingcar.model.io.OutputView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void gameStart() {
+        List<String> carList = inputView.setCarName();
+        int round = inputView.setCount();
+
+        List<RacingCar> racingCars = makeRacingCar(carList);
+        raceStart(racingCars, round);
+    }
+
+    private List<RacingCar> makeRacingCar(List<String> carList) {
+        String[] cars = carList.toArray(new String[0]);
+        List<RacingCar> racingCar = new ArrayList<>();
+        for (String car : cars) {
+            racingCar.add(new RacingCar(car));
+        }
+        return racingCar;
+    }
+
+    private void raceStart(List<RacingCar> racingCars, int round) {
+        System.out.println("실행 결과");
+        for (int i=0; i < round; i++) {
+            for (RacingCar racingCar : racingCars) {
+                racingCar.move();
+                outputView.printRoundResult(racingCar.getName(), racingCar.getDistance());
+            }
+            System.out.println();
+        }
+        outputView.printRaceResult(racingCars);
+    }
+}
