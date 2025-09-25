71bdcae66fb4e52c6596080f9bcc33c17b1114eb
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-09-25 17:30:54 +0900

    fix(view): 오타 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -17,7 +17,7 @@ public class InputView {
     }
 
     public static int readRounds() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println("시도할 횟수는 몇회인가요?");
         return Integer.parseInt(Console.readLine().trim());
     }
 }
