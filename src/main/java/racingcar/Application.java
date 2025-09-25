a6a0e8c20baf7bb353a241b3d392426d487da39d
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-09-25 10:40:31 +0900

    feat(app): Application 메인에서 콘솔 입력으로 Player 생성/출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import racingcar.player.Player;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Player player = Player.fromConsole();
+
+        System.out.println("cars = " + player.getCars());
+        System.out.println("rounds = " + player.getRounds());
+
+        /*
+        입력
+        pobi,woni,jun
+        5
+         */
+
+        /*
+        출력
+        cars = [pobi, woni, jun]
+        rounds = 5
+         */
+
     }
 }
