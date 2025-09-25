5e6ec2d768466030d761f2c30d6f428de798aaf9
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-09-25 17:38:25 +0900

    feat(app): Application 로직 단순화 및 테스트 성공
    - main() 메서드에서 GameManager가 게임을 개시

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,22 +1,11 @@
 package racingcar;
 
-import racingcar.player.Player;
-import racingcar.view.InputView;
-
-import java.util.List;
+import racingcar.manager.GameManager;
 
 public class Application {
     public static void main(String[] args) {
-        List<String> cars = InputView.readCars();
-        int rounds = InputView.readRounds();
-        Player player = Player.of(cars, rounds);
 
-        System.out.println("cars = " + player.getCars());
-        System.out.println("rounds = " + player.getRounds());
-
-        /*
-        cars = [pobi, woni, jun]
-        rounds = 5
-         */
+        GameManager gameManager = new GameManager();
+        gameManager.start();
     }
 }
