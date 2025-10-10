4943eeef8fe917ca76e86c37d9ca11a13f9882f1
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-10-11 02:19:32 +0900

    refactor(app): 의존 관계 리팩토링
    - Application에서 InputView/OutputView/Generator/MovePolicy 생성
    - GameManager에 DI로 주입 후 start() 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,21 @@
 package racingcar;
 
-import racingcar.manager.GameManager;
+import racingcar.controller.GameManager;
+import racingcar.generator.BasicGenerator;
+import racingcar.generator.Generator;
+import racingcar.policy.BasicMovePolicy;
+import racingcar.policy.MovePolicy;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
 
-        GameManager gameManager = new GameManager();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Generator generator = new BasicGenerator();
+        MovePolicy movePolicy = new BasicMovePolicy(generator);
+        GameManager gameManager = new GameManager(movePolicy, inputView, outputView);
         gameManager.start();
     }
 }
