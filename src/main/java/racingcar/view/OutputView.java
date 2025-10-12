6dd498c0bdceceea8e1dd739aaaf66bd1c89bd22
yim0327 <yim0327@naver.com>
2025-10-12 22:57:34 +0900

    style(outputview): 불필요한 개행 문자 제거

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -16,7 +16,7 @@ public class OutputView {
     }
 
     public void printWinners(List<String> winners) {
-        System.out.println("\n최종 우승자 : " + String.join(", ", winners));
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 
     public void printError(String errorMessage) {
